10fd26aab36cf0e2d3c5ad0361adb75be485be7f
hfhfff <dyftfboys@163.com>
2026-04-24 10:38:11 +0800

    同时按下K1和K2 LED一会点亮只按一个不点亮。

--- 3-1 独立按键控制LED亮灭/main.c
+++ 3-1 独立按键控制LED亮灭/main.c
@@ -1,46 +1,17 @@
 #include <regx52.h>
 
-void Delay1ms(unsigned int xms)		//@12.000MHz
-{
-	unsigned char i, j;
-    while (xms)
-    {
-        i = 2;
-        j = 239;
-        do
-        {
-            while (--j);
-        } while (--i);
-        xms--;
-    }
-    
-}
 void main()
 {
-    int i = 0;
     while (1)
     {
-        if (i <= 7)
+        if ((P3_1 == 0)&&(P3_0==0))
         {
-            if (P3_1 == 0)
-            {
-                P2 &= ~(1 << i);
-                
-            }
-            else
-            {
-                P2 |= ~(1 << i);//led off
-                
-            }
-            
-            
+            P2_0 = 0;
         }
         else
         {
-            i = 0;
+            P2_0 = 1;//led off
         }
-        
     }
-    i = i + 1;
-        Delay1ms(1000);
+    
 }

4d81edf4e6002e9419bb9ec82d39e3c2eff3e257
hfhfff <dyftfboys@163.com>
2026-05-10 14:51:54 +0800

    定时器中断先被注释掉,K1K2K3K44个按键可以分别控制0123 4个灯的反转

--- 7-1 按键控制LED流水灯模式/main.c
+++ 7-1 按键控制LED流水灯模式/main.c
@@ -2,40 +2,33 @@
 #include "delay.h"
 #include "LCD1602.h"
 #include "MatrixKey.h"
+#include "Timer0.h"
+#include "key.h"
 
-void Timer0_Init()
-{
-    TMOD = 0x01;//0000 0001
-    TF0 = 0;
-    TR0 = 1;
-    TH0 = 64535 / 256;
-    TL0 = 64535 % 256;
-    ET0 = 1;
-    EA = 1;
-    PT0 = 0;
-}
+unsigned char KeyNumber;
 void main()
 {
-    Timer0_Init();
+    // Timer0Init();
     while (1)
     {
-        
+        KeyNumber = Key();
+        if (KeyNumber == 1) P2_0 = ~P2_0;
+        if (KeyNumber == 2) P2_1 = ~P2_1;
+        if (KeyNumber == 3) P2_2 = ~P2_2;
+        if (KeyNumber == 4) P2_3 = ~P2_3;
     }
 }
 
 
-
-unsigned int T0Count;
-void Timer0_Rountine() interrupt 1
-{
-    TH0 = 64535 / 256;
-    TL0 = 64535 % 256;
-    T0Count++;
-    if (T0Count == 1000)
-    {
-        P2_0 = 0;
-        T0Count = 0;
-    }
-        
-
-}
+// void Timer0_Rountine() interrupt 1
+// {
+//     static unsigned int T0Count;
+//     TH0 = 0xFC;
+//     TL0 = 0x18;
+//     T0Count ++;
+//     if (T0Count == 1000)
+//     {
+//         P2_0 = ~P2_0;
+//         T0Count = 0;
+//     }
+// }

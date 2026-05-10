64231c0500be845c5a68fce0ecd7bbd28f27b618
hfhfff <dyftfboys@163.com>
2026-05-10 11:57:36 +0800

    定时性初始化好的初始化里面的溢出值是哦一毫秒之后才会制衣的累计加到一千毫秒第一个小灯会亮

--- 7-1 按键控制LED流水灯模式/main.c
+++ 7-1 按键控制LED流水灯模式/main.c
@@ -3,11 +3,39 @@
 #include "LCD1602.h"
 #include "MatrixKey.h"
 
+void Timer0_Init()
+{
+    TMOD = 0x01;//0000 0001
+    TF0 = 0;
+    TR0 = 1;
+    TH0 = 64535 / 256;
+    TL0 = 64535 % 256;
+    ET0 = 1;
+    EA = 1;
+    PT0 = 0;
+}
 void main()
 {
+    Timer0_Init();
     while (1)
     {
-        P2 = 0x00;
+        
     }
 }
-	
+
+
+
+unsigned int T0Count;
+void Timer0_Rountine() interrupt 1
+{
+    TH0 = 64535 / 256;
+    TL0 = 64535 % 256;
+    T0Count++;
+    if (T0Count == 1000)
+    {
+        P2_0 = 0;
+        T0Count = 0;
+    }
+        
+
+}

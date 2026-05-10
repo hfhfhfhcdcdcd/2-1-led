2bca96960f703579618bff2200e2fef88f6c0491
hfhfff <dyftfboys@163.com>
2026-05-10 15:23:23 +0800

    按下K1左边流水灯按K2右边流水灯

--- 7-1 按键控制LED流水灯模式/main.c
+++ 7-1 按键控制LED流水灯模式/main.c
@@ -4,31 +4,36 @@
 #include "MatrixKey.h"
 #include "Timer0.h"
 #include "key.h"
+#include <INTRINS.h>
 
-unsigned char KeyNumber;
+unsigned char KeyNumber,LEDMode;
 void main()
 {
-    // Timer0Init();
+    P2 = 0xfe;
+    Timer0Init();
     while (1)
     {
         KeyNumber = Key();
-        if (KeyNumber == 1) P2_0 = ~P2_0;
-        if (KeyNumber == 2) P2_1 = ~P2_1;
-        if (KeyNumber == 3) P2_2 = ~P2_2;
-        if (KeyNumber == 4) P2_3 = ~P2_3;
+        if (KeyNumber == 1) LEDMode = 1;
+        if (KeyNumber == 2) LEDMode = 2;
+        
+        // if (KeyNumber == 3) P2_2 = ~P2_2;
+        // if (KeyNumber == 4) P2_3 = ~P2_3;
+    }
+}  
+
+
+void Timer0_Rountine() interrupt 1
+{
+    static unsigned int T0Count;
+    TH0 = 0xFC;
+    TL0 = 0x18;
+    T0Count++;
+    
+    if (T0Count == 500)
+    {
+        T0Count = 0;
+        if (LEDMode == 1) { P2 = _crol_(P2, 1); }
+        if (LEDMode == 2) { P2 = _cror_(P2, 1); }
     }
 }
-
-
-// void Timer0_Rountine() interrupt 1
-// {
-//     static unsigned int T0Count;
-//     TH0 = 0xFC;
-//     TL0 = 0x18;
-//     T0Count ++;
-//     if (T0Count == 1000)
-//     {
-//         P2_0 = ~P2_0;
-//         T0Count = 0;
-//     }
-// }

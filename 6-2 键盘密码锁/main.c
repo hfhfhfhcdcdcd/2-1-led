6d84d78390c0c7d3c0a091795a9e519ba0f3592d
hfhfff <dyftfboys@163.com>
2026-05-10 10:12:28 +0800

    到目前为止跟老师显示的是一模一样的

--- 6-2 键盘密码锁/main.c
+++ 6-2 键盘密码锁/main.c
@@ -6,7 +6,7 @@
 void main()
 {
     unsigned char KeyNum = 0;
-    unsigned int PassWord;
+    unsigned int PassWord,Count;
     LCD_Init();
     LCD_ShowString(1, 1, "PassWord:");
     
@@ -15,11 +15,18 @@ void main()
         KeyNum = MatrixKey();
         if (KeyNum)
         {
-            if (KeyNum <= 10)
+            if (KeyNum <= 10)//判断1-10按键是否按下
             {
-                PassWord = KeyNum % 10;
+                if (Count < 4)
+                {
+                    PassWord *= 10;//密码左移一位，以便存储下一位密码
+                    PassWord += KeyNum % 10;//获取一位密码,其中10取余之后变成零所以密码一共是0到9
+                    Count++;
+                    LCD_ShowNum(2, 1, PassWord, 4);//更新显示密码
+                }
+
             }
-                LCD_ShowNum(2, 1, PassWord, 2);
+                
         }
         
         

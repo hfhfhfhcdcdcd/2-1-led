d6d4f3c860a62ecf1d924d8f9213ad9056f8d809
hfhfff <dyftfboys@163.com>
2026-05-10 09:53:09 +0800

    把用vs code开发C51的教程文件放在了6-2下面；矩阵键盘做的密码键盘，允许零到九这十个件有作用，其余键没作用

--- 6-2 键盘密码锁/main.c
+++ 6-2 键盘密码锁/main.c
@@ -1,14 +1,28 @@
 #include <regx52.h>
-
+#include "delay.h"
+#include "LCD1602.h"
+#include "MatrixKey.h"
 
 void main()
 {
-   
+    unsigned char KeyNum = 0;
+    unsigned int PassWord;
+    LCD_Init();
+    LCD_ShowString(1, 1, "PassWord:");
     
     while (1)
     {
-        P2 = 0xfe;
-    }
+        KeyNum = MatrixKey();
+        if (KeyNum)
+        {
+            if (KeyNum <= 10)
+            {
+                PassWord = KeyNum % 10;
+            }
+                LCD_ShowNum(2, 1, PassWord, 2);
+        }
         
+        
+    }
 }
 	

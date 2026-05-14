396445813ac9e876feeab4b16fb5fe41e41caaeb
hfhfff <dyftfboys@163.com>
2026-05-14 16:06:49 +0800

    第一行显示：年月日，星期，时分秒 第二行显示对应的数据

--- 10-2 DS1302时钟/main.c
+++ 10-2 DS1302时钟/main.c
@@ -14,12 +14,20 @@ void main()
     while (1)
     {
         DS1302_ReadTime();
-        LCD_ShowNum(1, 1,  DS1302_Time[0],2);
-        LCD_ShowNum(1, 4,  DS1302_Time[1],2);
-        LCD_ShowNum(1, 7,  DS1302_Time[2],2);
-        LCD_ShowNum(1, 10, DS1302_Time[3],2);
-        LCD_ShowNum(1, 13, DS1302_Time[4],2);
-        LCD_ShowNum(2, 1,  DS1302_Time[5],2);
-        LCD_ShowNum(2, 4,  DS1302_Time[6],2);
+        LCD_ShowString(1, 1, "Ye");
+        LCD_ShowString(1, 3, "Mo");
+        LCD_ShowString(1, 5, "Da");
+        LCD_ShowString(1, 7, "SU");
+        LCD_ShowNum(2, 1, DS1302_Time[0], 2);
+        LCD_ShowNum(2, 3,  DS1302_Time[1],2);
+        LCD_ShowNum(2, 5, DS1302_Time[2], 2);
+        LCD_ShowNum(2, 7, DS1302_Time[6], 2);
+        LCD_ShowString(1, 10, "Ho");
+        LCD_ShowString(1, 13, "Mi");
+        LCD_ShowString(1, 15, "Se");
+        LCD_ShowNum(2, 10, DS1302_Time[3], 2);//时
+        LCD_ShowNum(2, 13, DS1302_Time[4],2);//分
+        LCD_ShowNum(2, 15,  DS1302_Time[5],2);//秒
+        
     }
 }
